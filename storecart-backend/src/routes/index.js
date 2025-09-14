4cf83e9c6d5ceff51eecc2b50b5ba286b753c76e
sankarcodings <sankarcodings@gmail.com>
2025-09-14 15:41:41 +0530

    c

--- storecart-backend/src/routes/index.js
+++ storecart-backend/src/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import userRoutes from '../modules/user/user.routes.js';
-
+import {getAllData} from '../../getData.js'
 const router = Router();
 
 router.use('/users', userRoutes);
+router.use('/getData' , getAllData)
 
 export default router;
